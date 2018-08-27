edc7fb3503a6a8d58acb143dd6e63354d905a344
zhou151 <1045907858@qq.com>
2018-08-27 20:28:05 +0800

    --分页插件ok---

--- hibernate_hello/src/main/java/com/zhou/controller/UserController.java
+++ hibernate_hello/src/main/java/com/zhou/controller/UserController.java
@@ -20,9 +20,28 @@ public class UserController
 	
 	@ResponseBody
 	@RequestMapping(value="/index")
-	public  List<User> mvc10(@RequestParam("page") String page)
+	/**
+	 * 
+	 * @fun-name mvc10
+	 * @return-type List<User>
+	 * @author Mr_zhou
+	 * @date 2018年8月27日 下午7:52:11
+	 * @param page 多少页,不传为第一页
+	 * @return  
+	 * TODO queryForNewsPage参数2为每页显示多少数据
+	 */
+	public  NewsPage<User> mvc10(@RequestParam("page") String page)
 	{
-		NewsPage<User> page01 = userServiceIml.queryForNewsPage(Integer.valueOf(page), 5);
-		return page01.getList();
+		NewsPage<User> page01 = null;
+		int pageSize=3;
+		if(page==null||page.equals(""))
+		{
+			page01 =userServiceIml.queryForNewsPage(1, pageSize);
+		}
+		else {
+			page01 = userServiceIml.queryForNewsPage(Integer.valueOf(page), pageSize);
+		}
+		
+		return page01;
 	}
 }
--- hibernate_hello/src/main/java/com/zhou/service/NewsPage.java
+++ hibernate_hello/src/main/java/com/zhou/service/NewsPage.java
@@ -3,102 +3,134 @@ package com.zhou.service;
 import java.util.List;
 
 import com.zhou.dao.entity.User;
- 
-public class NewsPage<T> {
-    // 结果集
-    private List<T> list;
- 
-    // 查询记录总数
-    private int totalRecords;
- 
-    // 每页多少条记录
-    private int pageSize;
- 
-    // 第几页
-    private int pageNo;
- 
-    /**
-     * @return 总页数
-     * */
-    public int getTotalPages(){
-        return (totalRecords+pageSize-1)/pageSize;
-    }
- 
-    /**
-     * 计算当前页开始记录
-     * @param pageSize 每页记录数
-     * @param currentPage 当前第几页
-     * @return 当前页开始记录号
-     */
-    public int countOffset(int currentPage,int pageSize){
-        int offset = pageSize*(currentPage-1);
-        return offset;
-    }
- 
-    /**
-     * @return 首页
-     * */
-    public int getTopPageNo(){
-        return 1;
-    }
- 
-    /**
-     * @return 上一页
-     * */
-    public int getPreviousPageNo(){
-        if(pageNo<=1){
-            return 1;
-        }
-        return pageNo-1;
-    }
- 
-    /**
-     * @return 下一页
-     * */
-    public int getNextPageNo(){
-        if(pageNo>=getBottomPageNo()){
-            return getBottomPageNo();
-        }
-        return pageNo+1;
-    }
- 
-    /**
-     * @return 尾页
-     * */
-    public int getBottomPageNo(){
-        return getTotalPages();
-    }
- 
- 
-    public List<T> getList() {
-        return list;
-    }
- 
-    public void setList(List<T> list) {
-        this.list = list;
-    }
- 
-    public int getTotalRecords() {
-        return totalRecords;
-    }
- 
-    public void setTotalRecords(int totalRecords) {
-        this.totalRecords = totalRecords;
-    }
- 
-    public int getPageSize() {
-        return pageSize;
-    }
- 
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
- 
-    public int getPageNo() {
-        return pageNo;
-    }
- 
-    public void setPageNo(int pageNo) {
-        this.pageNo = pageNo;
-    }
+/**
+ * --------------------------------------
+ * @author Mr_zhou 2018年8月27日 下午8:13:27
+ * TODO hibernate分页插件封装
+ * -------------------------------------
+ *         bottomPageNo:2, ---- 尾页序号
+ *         nextPageNo:2,---- 下一页的序号
+ *         pageNo:1 ---- 当前页数字
+ *         pageSize:3, ---- 每页显示多少行数据
+ *         previousPageNo":1 -----上一页的序号
+ *         topPageNo:1,  -----首页序号
+ *         totalPages:2,  -----页数量
+ *         totalRecords:4 ----全部记录,总数据行数
+ *-------------------------------------*/
+public class NewsPage<T>
+{
+	// 结果集
+	private List<T> list;
+
+	// 查询记录总数
+	private int totalRecords;
+
+	// 每页多少条记录
+	private int pageSize;
+
+	// 第几页
+	private int pageNo;
+
+	/**
+	 * @return 总页数
+	 */
+	public int getTotalPages()
+	{
+		return (totalRecords + pageSize - 1) / pageSize;
+	}
+
+	/**
+	 * 计算当前页开始记录
+	 * 
+	 * @param pageSize
+	 *            每页记录数
+	 * @param currentPage
+	 *            当前第几页
+	 * @return 当前页开始记录号
+	 */
+	public int countOffset(int currentPage, int pageSize)
+	{
+		int offset = pageSize * (currentPage - 1);
+		return offset;
+	}
+
+	/**
+	 * @return 首页
+	 */
+	public int getTopPageNo()
+	{
+		return 1;
+	}
+
+	/**
+	 * @return 上一页
+	 */
+	public int getPreviousPageNo()
+	{
+		if (pageNo <= 1)
+		{
+			return 1;
+		}
+		return pageNo - 1;
+	}
+
+	/**
+	 * @return 下一页
+	 */
+	public int getNextPageNo()
+	{
+		if (pageNo >= getBottomPageNo())
+		{
+			return getBottomPageNo();
+		}
+		return pageNo + 1;
+	}
+
+	/**
+	 * @return 尾页
+	 */
+	public int getBottomPageNo()
+	{
+		return getTotalPages();
+	}
+
+	public List<T> getList()
+	{
+		return list;
+	}
+
+	public void setList(List<T> list)
+	{
+		this.list = list;
+	}
+
+	public int getTotalRecords()
+	{
+		return totalRecords;
+	}
+
+	public void setTotalRecords(int totalRecords)
+	{
+		this.totalRecords = totalRecords;
+	}
+
+	public int getPageSize()
+	{
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize)
+	{
+		this.pageSize = pageSize;
+	}
+
+	public int getPageNo()
+	{
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo)
+	{
+		this.pageNo = pageNo;
+	}
 }
